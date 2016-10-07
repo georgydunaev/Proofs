dfaaf8bb2e9e2d4e6b6b64db6ddd2aa0b824290a
Georgy Dunaev <georgedunaev@gmail.com>
2016-10-07 21:56:22 +0300

    2/4 complete

--- Yoneda.v
+++ Yoneda.v
@@ -2,23 +2,34 @@ Require Import HoTT.
 Require Import HoTT.Categories.
 Require Import HoTT.Categories.Functor.Prod.Core.
 Require Import HoTT.Categories.Category.Objects.
+
+Open Scope morphism.
+
+Theorem qwe
+(H : Funext)
+(C : Category)
+(F : Functor C set_cat)
+(A : C)
+(w : F A):
+(F _1 1)%morphism w = w.
+Proof.
+  simple refine (@happly _ _ _ _ _ _).
+  exact (@identity_of C set_cat F A).
+Defined.
+
+
 Section YonProo.
 Context `{Funext} (C : Category) (F : Functor C set_cat) (A : C).
-(*Context (u:(Core.object_of (induced_snd (hom_functor C) A) A) ->
-         (Core.object_of F A)).*)
-(*Context (w :(F _1 (m o x o 1))%morphism == (F _1 m)%morphism o ((F _1 x)%morphism)).*)
 Theorem Yoneda : Equiv (NaturalTransformation (induced_snd (hom_functor C) A) F) (F A).
 Proof.
 simple refine (@BuildEquiv _ _ _ _).
-intro g.
-destruct g.
-simple refine ((components_of A) _).
-simpl.
-exact (@identity C A).
-simpl.
+* intro g.
+  simple refine ((components_of g A) _).
+  simpl.
+  exact (@identity C A).
+* simpl.
 simple refine (@BuildIsEquiv _ _ _ _ _ _ _).
 intro u.
-Check Build_NaturalTransformation.
 + simple refine (@Build_NaturalTransformation _ _ _ _ _ _ ).
   simpl.
   intro X.
@@ -27,17 +38,25 @@ Check Build_NaturalTransformation.
   exact ((@Core.morphism_of _ _ F _ _ f) u).
   simpl.
   intros s d m.
+
   refine (@path_forall _ _ _ _ _ _).
-  intro x.
-  Check (F _1 (m o x o 1))%morphism .
+  intro j.
   simple refine (@happly _ _ _ _ _ _).
-  refine (F _1 (m o x o 1))%morphism.
+  refine (F _1 (m o j o 1))%morphism.
   reflexivity.
 + simpl.
-
+  intro w.
+  simpl.
+  refine (@qwe H C F A w). (*strange behaviour here*)
++ simpl.
+  intro w.
+  simpl.
 (*====stop here====*)
 
-(*  hott_simpl.
+(*
+  reflexivity.
+  destruct F.
+  hott_simpl.
   refine @funext_Op.*)
 
 Admitted.
@@ -46,9 +65,8 @@ End YonProo.
 (*Require Import Category.Core Category.Morphisms.
 Require Import HoTT.Tactics Trunc.
 Require Import HoTT.Categories.Category.Univalent.
-Require Import InitialTerminalCategory.*)
-
+Require Import InitialTerminalCategory.
 Check Equiv Unit Unit.
 Check Isomorphic.
 Check hom_functor C.
-Check induced_snd (hom_functor C) A.
+Check induced_snd (hom_functor C) A.*)
